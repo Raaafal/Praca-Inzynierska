6412b9d565ec718f8ecefe3ff434839668e5c1a0
Maciej Szreder <MSzreder3.14@wp.pl>
2021-07-20 11:26:32 +0200

    Gracz posiada domyślną logikę sterowania czasem ruchu
    * metoda WykonajRuch(LogikaPlanszy) implementuje logikę zarządzania
      czasem
    + pozostałyCzas informuje ile czawsu na wykonanie ruchu pozostało, dla
      ostatniej iteracji jest <=0
    + iteracja informuje o numerze obecnej iteracji

--- Assets/Gracz/Gracz.cs
+++ Assets/Gracz/Gracz.cs
@@ -2,6 +2,14 @@
 
 public class Gracz
 {
+    public class OgraniczeniaGracza
+    {
+        public float MaksCzasNaRuch = 0.5f;
+        public float MinCzasNaRuch = 0.5f;
+        public uint MinIteracji = 0;
+        public uint MaksIteracji = 0;//0 oznacza brak
+    }
+    public OgraniczeniaGracza Ograniczenia=new OgraniczeniaGracza();
     public PreferencjeGracza preferencjeGracza;
     public virtual int[] ObslugiwaneRozmiaryPlansz { get; } = new int[] { 3, 4, 5, 6, 8, 10 };
     public Color KolorKrolowej { get; set; } = new Color(1, 1, 1, 1);
@@ -10,9 +18,55 @@ public class Gracz
     {
         return BrakRuchu;
     }
+    private float pozostalyCzas = 0.5f;
+    private uint iteracja = 0;
+    public float PozostalyCzas=>pozostalyCzas;
+    public float Iteracja => iteracja;
+    (int x, int y) zatwierdzonyRuch = BrakRuchu;
     public virtual (int x, int y) WykonajRuch(LogikaPlanszy plansza)
     {
-        return WykonajRuch(plansza.Plansza);
+        if (iteracja < Ograniczenia.MaksIteracji || Ograniczenia.MaksIteracji == 0)
+        {
+            pozostalyCzas -= Time.deltaTime;
+            (int x, int y) ruch = WykonajRuch(plansza.Plansza);
+            if (ruch != BrakRuchu)
+            {
+                zatwierdzonyRuch = ruch;
+            }
+            ++iteracja;
+            if (pozostalyCzas < 0)
+            {
+                pozostalyCzas += Ograniczenia.MaksCzasNaRuch;
+                if (zatwierdzonyRuch == BrakRuchu)
+                {
+                    for(int x = 0; x < plansza.Wielkosc; ++x)
+                    {
+                        for (int y = 0; y < plansza.Wielkosc; ++y)
+                        {
+                            if (LogikaPlanszy.CzyWolne(plansza.Plansza[x][y]))
+                            {
+                                return (x, y);
+                            }
+                        }
+                    }
+                }
+                ruch = zatwierdzonyRuch;
+                zatwierdzonyRuch = BrakRuchu;
+                return ruch;
+            }
+        }
+        else
+        {
+            iteracja = 0;
+            pozostalyCzas = Ograniczenia.MaksCzasNaRuch;
+            return zatwierdzonyRuch;
+        }
+        return BrakRuchu;
+
+    }
+    bool CzyOstatniaTura()
+    {
+        return pozostalyCzas <= 0 || (Ograniczenia.MaksIteracji != 0 && iteracja >= Ograniczenia.MaksIteracji);
     }
     public bool czyNasluchujeKlikniec = false;
     public virtual void NasluchujKlikniec(int x, int y)
